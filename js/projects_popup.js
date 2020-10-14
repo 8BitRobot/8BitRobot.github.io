956d846c9a94a2c5e1970e4cb9b20f5d042dd45a
8BitRobot <premgiridhar11@gmail.com>
2020-10-13 22:09:27 -0700

    Made the popup show Skills Applied instead of separating them.

--- js/projects_popup.js
+++ js/projects_popup.js
@@ -16,8 +16,7 @@ let projects = {
     "bact": {
         class: "bact",
         img: "/img/projects_bigger/bact.png",
-        skillsAcquired: ["mongodb"],
-        skillsRefined: ["nodejs| (ExpressJS)", "webdev| (Media Queries)", "python| (BeautifulSoup)"],
+        skills: ["mongodb", "nodejs| (ExpressJS)", "webdev| (Media Queries)", "python| (BeautifulSoup)"],
         title: "Bay Area COVID-19 Tracker",
         description: [
             "The Bay Area COVID-19 Tracker was a website I developed with my team at Nize Systems. It automatically scrapes county health offices and Google News to display the latest information about the pandemic's effects in the Bay Area. We used Python's ```requests_html``` and ```BeautifulSoup``` libraries to develop the web scraper. The backend is written in Node.js using ExpressJS, and the frontend uses standard HTML, CSS, and JS (our struggles with developing this project prompted me to learn Vue.js for our next project, ChoreDash). We used MongoDB as the database to store county data to display on our Trends page.",
@@ -29,8 +28,7 @@ let projects = {
     "choredash": {
         class: "cd",
         img: "/img/projects_bigger/choredash.png",
-        skillsAcquired: ["vuejs"],
-        skillsRefined: ["webdev| (Axios, Google OAuth)", "python| (Falcon API)"],
+        skills: ["vuejs", "webdev| (Axios, Google OAuth)", "python| (Falcon API)", "mongodb"],
         title: "ChoreDash",
         description: [
             "ChoreDash was another website I developed with my team at Nize Systems. We created it to help people at high risk from COVID-19 have their needs met by enlisting the help of volunteers offering their services. Those in need could create posts indicating which chores or errands they needed done, and volunteers would offer help directly through our platform. We wrote the backend in Python using the Falcon API framework, and we used Vue.js for the frontend.",
@@ -41,8 +39,7 @@ let projects = {
     "studioheartengine": {
         class: "she",
         img: "/img/projects_bigger/she.png",
-        skillsAcquired: ["godot| (IN PROGRESS)"],
-        skillsRefined: ["webdev| (CSS Animations)", "sass"],
+        skills: ["webdev", "sass", "godot| (currently learning)"],
         title: "Studio Heart Engine",
         description: ["I'm the VP of Events at Studio Heart Engine, Foothill High School's largest game development club with over 40 members. Using the Godot Game Engine, we teach our club members the basics of storytelling, character and game design, music, and game development itself. Our goal is to have club members fully prepared to compete in a Game Jam at the end of the year, one that I'm responsible for planning and hosting.",
         "At the moment, the COVID-19 pandemic has negatively impacted many businesses, so I've put my event-planning and sponsorship-hunting to a halt. Instead, my primary contributions are in developing the club's official website."],
@@ -51,8 +48,7 @@ let projects = {
     "gigahacks": {
         class: "gh",
         img: "/img/projects/gigahacks.jpg",
-        skillsAcquired: [],
-        skillsRefined: ["python", "webdev"],
+        skills: ["python", "webdev", "java"],
         title: "Gigahacks",
         description: ["I'm one of the lead organizers of Gigahacks, an annual hackathon with over 200 total attendees and 20 international participants. So far, we've held an in-person event in 2019 and a virtual event in 2020. At our events, we've run workshops in Python, Java (at an AP Computer Science level), web development, and machine learning.",
         "My primary contributions were in running the workshops and mentoring the attendees. More specifically, I ran the Java and web development workshops, and I helped various participants come up with ideas, implement them, and make their product as good as it could be."],
@@ -61,8 +57,7 @@ let projects = {
     "falconhacks": {
         class: "fh",
         img: "/img/projects/falconhacks.png",
-        skillsAcquired: ["webdev"],
-        skillsRefined: ["python"],
+        skills: ["python", "webdev", "java"],
         title: "FalconHacks",
         description: ["I'm one of the lead organizers of FalconHacks, Foothill High School's largest local hackathon. At our virtual event in May 2020, we had over 45 participants. We've run workshops in Python, web development, app development (with Apache Cordova), and machine learning. We also started the FalconHacks Computing Contest, a challenge where contestants can solve computational problems modeled after the USA Computing Olympiad.",
         "My primary contributions were in writing problems for the Contest, running a web development workshop, and developing the hackathon's official website. This was actually my first major web development project; as such, though the website is functional, I have since learned much better practices than those I applied in that site."],
@@ -71,16 +66,14 @@ let projects = {
     "trivalleycoderdojo": {
         class: "tvcd",
         img: "/img/projects/tvcd.png",
-        skillsAcquired: ["java"],
-        skillsRefined: ["webdev", "python"],
+        skills: ["python", "webdev", "java"],
         title: "Tri-Valley Coder Dojo",
         description: ["Before the COVID-19 pandemic hit, I volunteered weekly as a programming mentor at the Tri-Valley Coder Dojo. The Coder Dojo is a weekly event where kids of all ages can come to work on their own projects and receive guidance from more experienced developers. I went there primarily to help kids with Python and web development, but I also started teaching myself Java to help the kids working on Java projects as well."]
     },
     "presence": {
         class: "pbn",
         img: "/img/projects/nize_presence.png",
-        skillsAcquired: ["nodejs"],
-        skillsRefined: ["python| (RC522, FastAPI)", "vuejs"],
+        skills: ["python| (RC522, FastAPI)", "nodejs", "vuejs", "sass"],
         title: "Presence by Nize",
         description: ["Presence was the first attendance system I worked on with my startup, Nize Systems. Essentially, it's a small wall-mounted device with an embedded RC522 RFID scanner. Students will have RFID cards, stickers, or keychains with data such as their name and ID number written to them, and they can scan their card on the device to check into class. Our system includes features to handle signing out of class (for example, to use the restroom) and digitized passes for students that arrive to class late or need to be excused, and it's highly customizable to the needs of a school.",
         "My primary contribution was in developing the software that runs on our Presence devices. Originally, I taught myself Node.js and used that to develop the device GUI and a websocket to make it communicate with some Python card-scanning code. However, after running some benchmarks, I determined that Vue.js would be more efficient, so I switched to Vue.js and an API written with Python and FastAPI."]
@@ -88,8 +81,7 @@ let projects = {
     "presenceremote": {
         class: "prbn",
         img: "/img/projects/nize_systems_llc.png",
-        skillsAcquired: ["sass"],
-        skillsRefined: ["python| (FastAPI)", "vuejs"],
+        skills: ["python| (FastAPI)", "vuejs", "sass"],
         title: "Presence Remote by Nize",
         description: ["When the COVID-19 pandemic hit, in-person school became distance learning, and our in-class Presence devices were rendered useless. However, we noticed that teachers were still struggling with attendance, only this time it was through video calls. In order to alleviate that, we developed Presence Remote. When teachers create Zoom meetings through the Presence Remote dashboard, it access the Zoom APIs to monitor attendance automatically. On the Presence Remote dashboard, teachers can view live updates of students leaving and entering the meeting, making the attendance process a lot simpler.",
         "My primary contribution was in the front-end. Using Vue.js and Sass, a styling language extremely new to me at the time, a colleague and I developed the entire web dashboard and implemented the live Zoom attendance roster."]
@@ -133,11 +125,13 @@ let loadPopup = function () {
         Delete all the P elements from this div
         then loop through my list and add a P element for each element in the list
     */
-    let skillsAcquired = document.getElementById("acquired-skills");
-    document.querySelectorAll("#acquired-skills > p").forEach(el => el.remove());
+    let skillsDiv = document.getElementById("skills");
+    console.log(skillsDiv);
+    document.querySelectorAll("#skills > p").forEach(el => el.remove());
     
-    for (let i of projects[hash].skillsAcquired) {
+    for (let i of projects[hash].skills) {
         let elTextList = i.split("|");
+        console.log(elTextList);
 
         let para = document.createElement("p");
         let img = document.createElement("img");
@@ -146,24 +140,7 @@ let loadPopup = function () {
         para.appendChild(img);
         let text = document.createTextNode(skills[elTextList[0]][0] + elTextList.slice(1).join("|"));
         para.appendChild(text);
-        skillsAcquired.appendChild(para);
-    }
-
-    // same as above, but a different list
-    let skillsRefined = document.getElementById("refined-skills");
-    document.querySelectorAll("#refined-skills > p").forEach(el => el.remove());
-
-    for (let i of projects[hash].skillsRefined) {
-        let elTextList = i.split("|");
-
-        let para = document.createElement("p");
-        let img = document.createElement("img");
-
-        img.src = skills[elTextList[0]][1];
-        para.appendChild(img);
-        let text = document.createTextNode(skills[elTextList[0]][0] + elTextList.slice(1).join("|"));
-        para.appendChild(text);
-        skillsRefined.appendChild(para);
+        skillsDiv.appendChild(para);
     }
     
     /*
